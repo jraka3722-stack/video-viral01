bc734e7732e53255ff78486103853dab9d7271d3
jraka3722-stack <jraka3722@gmail.com>
2026-06-20 23:49:10 +0700

    Implement video download and replay functions
    Added functions to download video and replay it.

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const videoCard = document.querySelector(".video-card");
 searchInput.addEventListener("keyup", () => {
 
 const keyword = searchInput.value.toLowerCase();
-
-const title =
-videoCard.dataset.title.toLowerCase();
+const title = videoCard.dataset.title.toLowerCase();
 
 if(title.includes(keyword)){
 videoCard.style.display = "block";
@@ -15,3 +13,16 @@ videoCard.style.display = "none";
 }
 
 });
+
+function downloadVideo(){
+window.open(
+"https://cdn2.sliwtdrive.com/BIYDvcux1.mp4",
+"_blank"
+);
+}
+
+function playAgain(){
+const video = document.getElementById("mainVideo");
+video.currentTime = 0;
+video.play();
+}
